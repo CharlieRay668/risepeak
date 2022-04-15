167fa5726d039c9d0ebf8b0acf0ab9177a5c3df4
Charlie Ray <charlie.ray84@gmail.com>
2022-04-14 18:07:37 -0600

    staff only

--- base/urls.py
+++ base/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path("", views.alone, name="alone"),
     path("home/", views.base, name="home"),
+    path("staff/", views.staff_only, name="staff_only"),
     path("student/<str:student_name>/", views.student, name="student"),
     path("staff/<str:staff_pass>/", views.staff, name="staff")
 ]
